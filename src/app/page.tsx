be74cdf49fd628a772745d4085d58cd5280a8faf
Nathan <nathan.mwai@student.moringaschool.com>
2024-11-01 08:52:48 +0300

    Learnt how to make my web responsive if i'm using grid. That way it will work on both phone as well as on laptops and desktops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       {/* Grids starts here */}
       <div className="h-screen text-white Parent bg-slate-900">
         <div className="container mx-auto">
-          <div className="grid grid-cols-3 gap-1">
+          <div className="grid lg:grid-cols-3 gap-1 md:grid-cols-2">
             <div className="bg-sky-500 p-6 rounded-lg">1</div>
             <div className="bg-sky-500 p-6 rounded-lg">2</div>
             <div className="bg-sky-500 p-6 rounded-lg">3</div>
