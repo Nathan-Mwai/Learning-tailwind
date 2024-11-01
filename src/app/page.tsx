3f28014d8f9877e2807ad05399bc25a3cfd7f832
Nathan <nathan.mwai@student.moringaschool.com>
2024-11-01 08:12:05 +0300

    learning how to use flex wrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,8 +40,13 @@ export default function Home() {
       <div className="Parent">
         <div>Header</div>
         <div className="flex flex-col md:flex-row">
-          <div className="md:w-32 bg-fuchsia-600">Sidebar</div>
-          <div className="w-full bg-teal-600">Main content</div>
+          <div className="flex flex-wrap w-full">
+            <div className="w-32 grow">01</div>
+            <div className="w-32 grow">02</div>
+            <div className="w-32 grow">03</div>
+            <div className="w-32 grow">04</div>
+            <div className="w-32 grow">05</div>
+          </div>
         </div>
       </div>
     </div>
